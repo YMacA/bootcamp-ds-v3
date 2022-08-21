9a8227855ce2d8ba925164539d6388c6018ba24b
oscar <oscar.rondo12@gmail.com>
2022-08-20 20:24:00 -0400

    1st commit

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ st.text('''
 3. Merge 2 datasets
  ''')
 
-df_expected = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Expected.csv", encoding="latin-1", dtype=str)
+#df_expected = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Expected.csv", encoding="latin-1", dtype=str)
 
-df_counted = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Counted.csv", encoding="latin-1", dtype=str)
+#df_counted = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Counted.csv", encoding="latin-1", dtype=str)
 
 
-st.dataframe(df_expected.sample(2).T)
+#st.dataframe(df_expected.sample(2).T)
