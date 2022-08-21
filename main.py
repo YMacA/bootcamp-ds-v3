9d3e5243a146b495c69cc81a1a55b8ae61abe38d
oscar <oscar.rondo12@gmail.com>
2022-08-20 20:16:45 -0400

    1st commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+import streamlit as st
+#from pandasql import sqldf
+st.title('Exercise 3 (Inventory Discrepancy)')
+st.text('''
+1. Remove dups
+2. Aggregate
+3. Merge 2 datasets
+ ''')
+
+ df_expected = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Expected.csv", encoding="latin-1", dtype=str)
+
+ df_counted = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Counted.csv", encoding="latin-1", dtype=str)
+
+
+ st.dataframe(df_expected.sample(2).T)
+ 
