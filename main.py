8f5688a59d30c91774fcce631ca49c9417e5778b
oscar <oscar.rondo12@gmail.com>
2022-08-20 20:33:04 -0400

    1st commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ st.text('''
 
 df_expected = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Expected.csv", encoding="latin-1", dtype=str)
 
-#df_counted = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Counted.csv", encoding="latin-1", dtype=str)
+df_counted = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Counted.csv", encoding="latin-1", dtype=str)
 
 
-#st.dataframe(df_expected.sample(2).T)
+st.dataframe(df_expected.sample(2).T)
