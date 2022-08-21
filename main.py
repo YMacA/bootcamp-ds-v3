ad360ec83ed70e8d7c1d512544c41b7ecc11ca0a
oscar <oscar.rondo12@gmail.com>
2022-08-20 21:34:46 -0400

    1st commit

--- main.py
+++ main.py
@@ -9,20 +9,47 @@ st.text('''
 3. Merge 2 datasets
  ''')
 
+
+
 df_expected = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Expected.csv", encoding="latin-1", dtype=str)
 
 df_counted = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Counted.csv", encoding="latin-1", dtype=str)
 
+code = '''
+df_expected = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Expected.csv", encoding="latin-1", dtype=str)
+
+df_counted = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Counted.csv", encoding="latin-1", dtype=str)
+
+'''
+st.code(code, language='python')
+
+code = '''
+df_expected.sample(2).T
+'''
+st.code(code, language='python')
 
 st.dataframe(df_expected.sample(2).T)
 #df_counted.shape()
 #df_counted['RFID'].nunique()
+code = '''
 
 df_counted = df_counted.drop_duplicates("RFID")
 
 df_B = df_counted.groupby("Retail_Product_SKU").count()[["RFID"]].reset_index().rename(columns={"RFID":"Retail_CCQTY"})
 
+'''
+st.code(code, language='python')
+
+df_counted = df_counted.drop_duplicates("RFID")
+
+df_B = df_counted.groupby("Retail_Product_SKU").count()[["RFID"]].reset_index().rename(columns={"RFID":"Retail_CCQTY"})
+
+code = '''
 df_B.sample(10)
+'''
+st.code(code, language='python')
+
+st.dataframe(df_B.sample(10))
 
 my_cols_selected = ["Retail_Product_Color",
 "Retail_Product_Level1",
@@ -36,8 +63,21 @@ my_cols_selected = ["Retail_Product_Color",
 "Retail_Product_Style",
 "Retail_SOHQTY"]
 
+code = '''
+my_cols_selected = ["Retail_Product_Color",
+"Retail_Product_Level1",
+"Retail_Product_Level1Name",
+"Retail_Product_Level2Name",
+"Retail_Product_Level3Name",
+"Retail_Product_Level4Name",
+"Retail_Product_Name",
+"Retail_Product_SKU",
+"Retail_Product_Size",
+"Retail_Product_Style",
+"Retail_SOHQTY"]
 
-
+'''
+st.code(code, language='python')
 
 df_A = df_expected[my_cols_selected]
 
@@ -64,7 +104,7 @@ st.dataframe(df_discrepancy.head())
 df_discrepancy.loc[df_discrepancy["Diff"]<0, "Unders"] = df_discrepancy["Diff"] * (-1)
 
 
-st.code(df_discrepancy["Unders"] = df_discrepancy["Unders"].fillna(0).astype(int),language='python' )
+df_discrepancy["Unders"] = df_discrepancy["Unders"].fillna(0).astype(int)
 
 
 st.dataframe(df_discrepancy.sample(10))
@@ -75,15 +115,12 @@ st.dataframe(df_discrepancy.describe())
 
 #st.write(df_discrepancy.shape())
 
-code = '''def hello():
-     print("Hello, Streamlit!")'''
-st.code(code, language='python')
 
 df_discrepancy[df_discrepancy["Diff"].isnull()]
-code2 = '''
+code = '''
 df_discrepancy[df_discrepancy["Diff"].isnull()]
 '''
-st.code(code2, language='python')
+st.code(code, language='python')
 
 
 
